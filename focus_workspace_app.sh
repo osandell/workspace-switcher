ce14af163b546131ce261af272a51e9488e4a1f1
Olof Sandell <83674246+osandell@users.noreply.github.com>
2024-03-03 12:25:12 +0100

    fix: always activate top bar when switching to workspace

--- focus_workspace_app.sh
+++ focus_workspace_app.sh
@@ -4,13 +4,16 @@ focused_workspace_app=$(cat /tmp/focused_workspace_app.txt)
 
 case $focused_workspace_app in
 'kitty-main')
+    open -a 'Electron'
     open -a 'Visual Studio Code'
     open -a 'kitty-main'
     ;;
 'kitty-lazygit')
+    open -a 'Electron'
     open -a 'kitty-lazygit'
     ;;
 'vscode')
+    open -a 'Electron'
     open -a 'kitty-main'
     open -a 'Visual Studio Code'
     ;;
